b42097fcf2d7c0f6a2f4418eefcf47cea10eaee2
Admin <Admin@DESKTOP-E7OIGPE>
2025-05-19 19:25:44 +0500

    update classes

--- ConsoleApplication4/Matrix.hpp
+++ ConsoleApplication4/Matrix.hpp
@@ -1,66 +1,280 @@
-#pragma once
-#ifdef MATRIX_HPP
-#endif MATRIX_HPP
+//шаблонные классы
+// шаблон целочисленной матрицы
+
+
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
 
 #include <iostream>
 #include <type_traits>
 #include <numeric>
 #include <initializer_list>
 
-//Это шаблоные класс матрицы который должен уметь работать ТОЛЬКО с целыми числами
-// в качестве хранимых типов. Для этого вам надо изучить тему assert
+//это шаблонный класс матрицы который должен уметь работать ТОЛЬКО с целыми числами
+// в качестве хранимых типов. Для этого вам надо изучить тему assert функций,
 // специализацию шаблонов, или constraint шаблонов. Любой из этих способов может
-// ограничивать область применения вашего шаблона
-template<typename Type, unsigned long long Coll, unsigned long long Row>
+// ограничить область применения вашего шаблона
+template < typename Type, unsigned long long Coll, unsigned long long Row>
 class Matrix {
 public:
-	Matrix();
+	Matrix() {}
+
 	Matrix(/*набор параметров который вы можете сюда передать*/);
-	Matrix(const Matrix& other);
-	Matrix(Matrix&& dead);
-	~Matrix();
 
-	const Matrix& operator=(const Matrix& other);
-	const Matrix& operator=(Matrix& other);
+	 Matrix(const Matrix& other)
+	 {
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 data_[i][j] = other.data_[i][j];
+			 }
+		 }
+	 }
 
-	//operator[]??? саечка за то что-бы реализовать это без В           д е н и с ь к а  п и п и с ь к а
-
-	friend const Matrix& operator+(const Matrix& lso, const Matrix& rso);
-	friend const Matrix& operator-(const Matrix& lso, const Matrix& rso);
-	friend const Matrix& operator*(const Matrix& lso, const Matrix& rso);
-	friend const Matrix& operator/(const Matrix& lso, const Matrix& rso);
-	friend const Matrix& operator^(const Matrix& lso, const Matrix& rso);
-
-	friend const Matrix& operator+=(const Matrix& lso, const Matrix& rso);
-	friend const Matrix& operator-=(const Matrix& lso, const Matrix& rso);
-	friend const Matrix& operator*=(const Matrix& lso, const Matrix& rso);
-	friend const Matrix& operator/=(const Matrix& lso, const Matrix& rso);
-	friend const Matrix& operator^=(const Matrix& lso, const Matrix& rso);
+	 ~Matrix()
+	 {
+		 default;
+	 }
 	
-	friend std::ostream& operator<<(std::ostream& outs, const Matrix& rso);
-	friend std::istream& operator>>(std::istream& ins, const Matrix& rso);
+	 const Matrix& operator=(const Matrix& other)
+	 {
+		 this->date_, other.data_, sizeof(Type)* Row* Coll;
+		 return *this;
+	 }
+
+	 const Matrix& operator=(Matrix&& other)
+	 {
+
+	 }
+	
+	 Type& at(unsigned long long Row, unsigned long long Coll)
+	 {
+		 return data_[Row][Coll];
+	 }
+
+	 const Type& at(unsigned long long Row, unsigned long long Coll) const
+	 {
+		 return data_[Row][Coll];
+	 }
+		
+	
+	 friend const Matrix& operator+ (const Matrix<Type, Row, Coll>& lso)
+	 {
+		 Matrix<Type, Row, Coll> result;
+
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 result[i][j] = lso[i][j] + rso[i][j];
+			 }
+		 }
+
+		 return result;
+	 }
+
+	
+	 friend const Matrix& operator- (const Matrix<Type, Row, Coll>& lso)
+	 {
+		 Matrix<Type, Row, Coll> result;
+
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 result[i][j] = lso[i][j] - rso[i][j];
+			 }
+		 }
+
+		 return result;
+	 }
+
+	 friend const Matrix& operator* (const Matrix<Type, Row, Coll>& lso)
+	 {
+		 Matrix<Type, Row, Coll> result;
+
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 result[i][j] = 0;
+
+				 for (int t = 0; t < Coll; t++)
+				 {
+					 result[i][j] += lso[i][t] * rso[t][j];
+				 }
+			 }
+		 }
+
+		 return result;
+	 }
 
 
+	 friend const Matrix& operator/ (const Matrix<Type, Row, Coll>& lso)
+	 {
+		 Matrix<Type, Row, Coll> result;
+
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 result[i][j] = 0;
+
+				 for (int t = 0; t < Coll; t++)
+				 {
+					 result[i][j] += lso[i][t] * (1 / rso[t][j]);
+				 }
+			 }
+		 }
+
+		 return result;
+	 }
+
+	 friend const Matrix & operator^(const Matrix & lso, const Integer & rso);
+	 {
+		 Matrix<Type, Row, Coll> result;
+
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 result[i][j] = 0;
+
+				 for (int t = 0; t < Coll; t++)
+				 {
+					 result[i][j] += lso[i][t] * (1 / rso[t][j]);
+				 }
+			 }
+		 }
+
+		 return result;
+	 }
+	
+		
+	 friend const Matrix& operator=+(const Matrix & lso, const Matrix & rso)
+	 {
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 lso[i][j] =+ rso[i][j];
+			 }
+		 }
+
+		 return lso;
+	 }
+
+	 friend const Matrix& operator=-(const Matrix & lso, const Matrix & rso)
+	 {
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 lso[i][j] =- rso[i][j];
+			 }
+		 }
+
+		 return lso;
+	 }
+	 friend const Matrix& operator=*(const Matrix& lso, const Matrix& rso)
+	 {
+		 Matrix<Type, Row, Coll> result;
+
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 result[i][j] = 0;
+				 for (int t = 0; t < Coll; t++)
+				 {
+					 result[i][j] =+ lso[i][t] * rso[t][j];
+				 }
+			 }
+		 }
+		 lso = result;
+
+		 return lso;
+	 }
+	 friend const Matrix& operator= / (const Matrix & lso, const Matrix & rso)
+	 {
+		 Matrix<Type, Row, Coll> result;
+
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 result[i][j] = 0;
+
+				 for (int t = 0; t < Coll; t++)
+				 {
+					 result[i][j] += lso[i][t] * (1 / rso[t][j]);
+				 }
+			 }
+		 }
+		 lso = result;
+
+		 return lso;
+	 }
+
+	 friend std::ostream& operator<<(std::ostream& outs, const Matrix& rso)
+	 {
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 outs << rso[i][j] << " ";
+			 }
+			 outs << "\n";
+		 }
+		 return outs;
+	 }
+
+	 friend std::istream& operator>>(std::istream& ins, const Matrix& rso)
+	 {
+		 for (int i = 0; i < Row; i++)
+		 {
+			 for (int j = 0; j < Coll; j++)
+			 {
+				 ins >> rso[i][j] >> " ";
+			 }
+		 }
+		 return ins;
+	 }
+
+	Type begin() { return data_; }
+	Type end() { return data_ + (Row * Coll); }
+	
 private:
-	Type date_[Coll][Row];
+	Type data_[Coll][Row];
+	
 };
 
-namespace std {
-	template<>
-	struct hash<Matrix<Type, unsigned long long, unsigned long long>> // Это надо исправить
+namespace std
+{
+
+	template<typename Type, unsigned long long Row, unsigned long long Coll>
+	struct hash<Matrix<Type, Row, Coll>>
 	{
-		size_t  operator()(Matrix<Type, unsigned long long, unsigned long long>& obj)
+		size_t operator()(const Matrix<Type, unsigned long long, unsigned long long>&
+			obj) {
 			size_t result{};
 			for (size_t i = 0; i < obj.rows(); i++)
 			{
-				for (size_t i = 0; i < obj.columns; i++)
+				for (size_t j = 0; j < obj.columns(); j++)
 				{
 					result <<= 1;
 					result ^= hash<Type>()(obj[i][j]);
 				}
 			}
 			return result;
+		};
+
 	};
-	//Стркутура данного трейта-хеширования полностью корректна
-	//наша задача исправить
 }
+
+#endif
+//до пятницы нужно реализовать это хотябы в попытке работоспособности
+//испорльзовать можно только те библиотеки которые прописаны сверху и ваши
+// классы целых чисел из предыдущего задания(для сверхразумов ещё и rational+real)
+// First lectule in friday will be comprtition ow yours clown solutio;
